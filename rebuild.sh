ea5ceecefc91da83b750949923484bfcefe0ace7
Robert Wiggins <51722811+RandomRobbieBF@users.noreply.github.com>
2020-08-19 13:16:18 +0100

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -3,5 +3,5 @@ git pull
 docker-compose down -v
 docker images purge -a
 docker-compose up -d
-#sleep 35
-#docker exec splunkenterprise bash -c "chown -R splunk:splunk /tomcat-container/ && rm /usr/local/tomcat/webapps/ROOT/index.html"
+sleep 35
+docker exec splunkenterprise bash -c "chown -R splunk:splunk /tomcat-container/ && rm /usr/local/tomcat/webapps/ROOT/index.html"
